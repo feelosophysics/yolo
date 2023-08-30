50a5bef1969c3ff0edd51a616bf35875d6eb7ff3
charlie <f22losophysics@gmail.com>
2023-08-30 16:30:43 +0900

    tnwjd

--- yolov8/dogcat.v1i.yolov8/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# dogcat > 2023-08-16 11:17am
-https://universe.roboflow.com/metaverse-academy-jlwtr/dogcat-pnbpw
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- yolov8/yoloinfer.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('runs/detect/train/weights/best.pt')
-result = model.predict(source='fish.jpg', show= False, save = True)
-
-results=
