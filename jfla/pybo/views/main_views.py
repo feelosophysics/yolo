a86cbce225b5e276c0ce5f226802dea71049ef07
charlie <f22losophysics@gmail.com>
2023-08-30 17:07:03 +0900

    create rand

--- jfla/pybo/views/main_views.py
+++ jfla/pybo/views/main_views.py
@@ -3,11 +3,17 @@ from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+import random
+from flask import render_template
 
 @bp.route('/hello')
 def hello_pybo():
-    return 'Hello, Pybo!'
 
+    abab = random.randint(1, 100)
+    print(abab)
+    print(3)
+
+    return render_template('question/new.html', club = abab)
 
 @bp.route('/')
 def index():
